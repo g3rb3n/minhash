89467b0615400cb07526e39a3518d591125d87e6
Gerben <gerbenvaneerten@gmail.com>
2017-06-10 13:36:53 +0200

    Fixed mail address

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(name='minhash',
       description='Minhash for text deduplication',
       url='http://github.com/g3rb3n/minhash',
       author='g3rb3n',
-      author_email='g3rb3n@gmail.com',
+      author_email='3grbn3@gmail.com',
       license='MIT',
       packages=['minhash'],
+      install_requires=['python-slugify==1.2.1']
       zip_safe=False)
