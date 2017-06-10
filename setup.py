a77d87ae867163952fa1bb0acf6c3657bd9da9c3
Gerben <gerbenvaneerten@gmail.com>
2017-06-10 13:30:47 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='minhash',
+      version='0.1',
+      description='Minhash for text deduplication',
+      url='http://github.com/g3rb3n/minhash',
+      author='g3rb3n',
+      author_email='g3rb3n@gmail.com',
+      license='MIT',
+      packages=['minhash'],
+      zip_safe=False)
